01137999a4262f92618247598d4c487f141ee50f
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-31 19:58:09 -0800

    Add scope for current interns.

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,81 +1,99 @@
 module Api::V1
-class StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :update, :destroy]
+  class StudentsController < ApplicationController
+    before_action :set_student, only: [:show, :update, :destroy]
 
-  # GET /students
-  def index
-    if params[:query]
-      @students = Student.search(params[:query])
-      if @students.blank?
-        render status: 200, json: {
-          message: "No results found."
-        }
+    # GET /students
+    def index
+      if params[:query]
+        @students = Student.search(params[:query])
+        if @students.blank?
+          render status: 200, json: {
+            message: "No results found."
+          }
+        else
+          render json: @students
+        end
+      elsif params[:random]
+        @student = Student.all.sample
+        render json: @student
+      elsif params[:names]
+        @students = Student.all
+        render json: @students,  each_serializer: NamesSerializer
+      elsif params[:links]
+        @students = Student.all
+        render json: @students,  each_serializer: LinksSerializer
+      elsif params[:info]
+        @students = Student.all
+        render json: @students,  each_serializer: AboutSerializer
+      elsif params[:seeking]
+        @students = Student.seeking
+        if @students.blank?
+          render status: 200, json: {
+            message: "No job seekers or interns found."
+          }
+        else
+          render json: @students
+        end
+      elsif params[:intern]
+        @students = Student.intern
+        if @students.blank?
+          render status: 200, json: {
+            message: "No current interns found."
+          }
+        else
+          render json: @students
+        end
+      elsif params[:employed]
+        @students = Student.employed
+        if @students.blank?
+          render status: 200, json: {
+            message: "No employed students to show."
+          }
+        else
+          render json: @students
+        end
       else
+        @students = Student.all
         render json: @students
       end
-    elsif params[:random]
-      @student = Student.all.sample
+    end
+
+    # GET /students/1
+    def show
       render json: @student
-    elsif params[:names]
-      @students = Student.all
-      render json: @students,  each_serializer: NamesSerializer
-    elsif params[:links]
-      @students = Student.all
-      render json: @students,  each_serializer: LinksSerializer
-    elsif params[:info]
-      @students = Student.all
-      render json: @students,  each_serializer: AboutSerializer
-    elsif params[:seeking]
-      @students = Student.seeking
-      if @students.blank?
+    end
+
+    # POST /students
+    def create
+      @student = Student.new(student_params)
+      if @student.save
+        render json: @student, status: :created, location: v1_student_url(@student)
+      else
+        render json: @student.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /students/1
+    def update
+      if @student.update(student_params)
         render status: 200, json: {
-          message: "No results found."
+          message: "Student has been updated."
         }
       else
-      render json: @students
+        render json: @student.errors, status: :unprocessable_entity
+      end
     end
-    else
-    @students = Student.all
-    render json: @students
+
+    # DELETE /students/1
+    def destroy
+      if @student.destroy
+        render status: 200, json: {
+          message: "Student has been deleted."
+        }
+      end
     end
-  end
 
-  # GET /students/1
-  def show
-    render json: @student
-  end
-
-  # POST /students
-  def create
-    @student = Student.new(student_params)
-    if @student.save
-      render json: @student, status: :created, location: v1_student_url(@student)
-    else
-      render json: @student.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /students/1
-  def update
-    if @student.update(student_params)
-      render status: 200, json: {
-        message: "Student has been updated."
-      }
-    else
-      render json: @student.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /students/1
-  def destroy
-    if @student.destroy
-      render status: 200, json: {
-        message: "Student has been deleted."
-      }
-    end
-  end
-
-  private
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_student
       @student = Student.find(params[:id])
@@ -85,5 +103,5 @@ class StudentsController < ApplicationController
     def student_params
       params.require(:student).permit(:name, :linkedin, :github, :status, :bio)
     end
-end
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,21 @@
 class Student < ApplicationRecord
   validates :name, :linkedin, :github, :status, :bio, presence: true
+
   scope :search, -> (query) { where("name ilike ? or bio ilike ?", "%#{query}%", "%#{query}%") }
-  scope :seeking, -> do (
-    where("status ilike ?", 'seeking')
-  )
+
+scope :intern, -> do (
+  where("status like ?", 'intern')
+)
 end
-  end
+
+scope :seeking, -> do (
+  where("status ilike ? or status ilike ?", 'seeking', 'intern')
+)
+end
+
+scope :employed, -> do (
+  where("status ilike ?", 'employed')
+)
+end
+
+end
