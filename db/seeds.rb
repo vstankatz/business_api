66aa5b72e5aae0f749219fc88b912ebd261aba29
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-01-31 16:18:45 -0800

    Add first two student seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,107 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+
+Student.create([{
+  name: "Angela Weber",
+  github: "https://github.com/web1221",
+  linkedin: "https://www.linkedin.com/in/angelaweber95/",
+  status: "",
+  bio: ""
+},
+{
+  name: "Celeste Morton",
+  github: "https://github.com/Celesterenee7",
+  linkedin: "https://www.linkedin.com/in/celestemorton/",
+  status: "",
+  bio: ""
+},
+{
+  name: "Courtney Schild",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Eliza Sohn",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Eva Wiedmann",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Joann Carter",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Lara Bjork",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Lela Smith",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Lillian Theuma",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Maria Ivy",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+},
+{
+  name: "Veronica Stanley-Katz",
+  github: "",
+  linkedin: "",
+  status: "",
+  bio: ""
+}
+# {
+#   name: "",
+#   github: "",
+#   linkedin: "",
+#   status: "",
+#   bio: ""
+# },
+# {
+#   name: "",
+#   github: "",
+#   linkedin: "",
+#   status: "",
+#   bio: ""
+# },
+# {
+#   name: "",
+#   github: "",
+#   linkedin: "",
+#   status: "",
+#   bio: ""
+# },
+
+  ])
+
+p "Created #{Student.count} students created!"
